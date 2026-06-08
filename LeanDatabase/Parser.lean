f8a57e093ae0478227a9ad2831355ada802d72fa
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-06-08 16:09:47 +0530

    parser

---
+++ LeanDatabase/Parser.lean
@@ -0,0 +1,31 @@
+import Lean
+
+open Lean Meta Elab Term
+
+namespace LeanDatabase
+
+def elabFilter (schema : List (Name × Name)) (stx : Syntax) : TermElabM Expr := do
+  match schema with
+  | [] => elabTerm stx none
+  | (name, colType) :: rest => do
+    let colTypeExpr ←  Term.mkConst colType
+    withLocalDeclD name colTypeExpr fun localVar => do
+      let restExpr ← elabFilter rest stx
+      mkLambdaFVars #[localVar] restExpr
+
+def parseFilter (schemaStr : List (String × String)) (str : String) : TermElabM Expr := do
+  let .ok stx := Parser.runParserCategory (← getEnv) `term str | throwError "Failed to parse filter expression: {str}"
+  let schema := schemaStr.map (fun (name, colType) => (name.toName, colType.toName))
+  elabFilter schema stx
+
+def egFilter := parseFilter [("age", "Int"), ("isActive", "Bool")] "age > 30 && isActive"
+
+elab "egfilter%" : term => do
+  let e ← egFilter
+  return e
+
+#check egfilter%
+
+#eval egfilter% 32 true
+
+end LeanDatabase
