6a8484f0d10f233048eb224d95f460c9251835a8
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-06-11 09:51:04 +0530

    experiments

--- LeanDatabase/Parser.lean
+++ LeanDatabase/Parser.lean
@@ -12,28 +12,61 @@ namespace LeanDatabase
 
 Since SQL types are all Lean constants, we represent them by names.
 -/
-def elabFilter (schema : List (Name × Name)) (stx : Syntax) : TermElabM Expr := do
+def elabFilter' (schema : List (Name × Name)) (stx : Syntax) : TermElabM Expr := do
   match schema with
   | [] => elabTermEnsuringType stx (mkConst ``Bool)
   | (name, colType) :: rest => do
     let colTypeExpr ←  Term.mkConst colType
     withLocalDeclD name colTypeExpr fun localVar => do
+      let restExpr ← elabFilter' rest stx
+      mkLambdaFVars #[localVar] restExpr
+
+inductive SqlTargetType where
+  | int
+  | bool
+  | float
+  | string
+
+def typeExpr (t : SqlTargetType) : Expr :=
+  match t with
+  | .int => mkConst ``Int
+  | .bool => mkConst ``Bool
+  | .float => mkConst ``Rat
+  | .string => mkConst ``String
+
+def normalizeSQLType (sqlType : String) : SqlTargetType :=
+  let s := sqlType.toLower
+  if s.startsWith "varchar" then .string
+  else if s.startsWith "int" then .int
+  else if s.startsWith "bool" then .bool
+  else if s.startsWith "float" then .float
+  else if s.startsWith "text" then .string
+  else if s.startsWith "char" then .string
+  else .string -- default to string for unrecognized types
+
+def elabFilter (schema : List (Name × SqlTargetType)) (stx : Syntax) : TermElabM Expr := do
+  match schema with
+  | [] => elabTermEnsuringType stx (mkConst ``Bool)
+  | (name, colType) :: rest => do
+    let colTypeExpr := typeExpr colType
+    withLocalDeclD name colTypeExpr fun localVar => do
       let restExpr ← elabFilter rest stx
       mkLambdaFVars #[localVar] restExpr
 
-def normalizeSQLType (sqlType : String) : String :=
-  let s := sqlType.toLower
-  if s.startsWith "varchar" then "String"
-  else if s.startsWith "int" then "Int"
-  else if s.startsWith "bool" then "Bool"
-  else if s.startsWith "float" then "Float"
-  else if s.startsWith "text" then "String"
-  else if s.startsWith "char" then "String"
-  else s
+variable (p: Fin n → Type) [∀ i, DecidableEq (p i)]
+
+#synth DecidableEq ((i : Fin n) → p i)
+
+
+instance liftDecEq (p: Fin n → Type) [∀ i, DecidableEq (p i)] : DecidableEq ((i : Fin n) → p i) :=
+  inferInstance
+
+
+#check liftDecEq
 
 def parseFilter (schemaStr : List (String × String)) (str : String) : TermElabM Expr := do
   let .ok stx := Parser.runParserCategory (← getEnv) `term str | throwError "Failed to parse filter expression: {str}"
-  let schema := schemaStr.map (fun (name, colType) => (name.toName, (normalizeSQLType colType).toName))
+  let schema := schemaStr.map (fun (name, colType) => (name.toName, normalizeSQLType colType))
   elabFilter schema stx
 
 def egFilter := parseFilter [("age", "Int"), ("isActive", "Bool")] "age > 30 && isActive"
@@ -54,6 +87,12 @@ def egFilter' := parseFilter [("age", "Int"), ("isActive", "Bool")] "age > 30"
 elab "egfilter%%" : term => do
   let e ← egFilter'
   return e
+
+#check Fin.rec
+
+#check List.get
+
+#check Fin.succ
 
 -- #check egfilter%%
 
